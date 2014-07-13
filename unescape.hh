f733ee2dedc49f2a15bfa8c493f7aa2930df6165
Helmuth Ploner <h.ploner@axis-simulation.com>
2014-07-13 14:16:24 +0200

    added documentation

--- unescape.hh
+++ unescape.hh
@@ -4,6 +4,15 @@ namespace JSON {
 
     namespace helper {
 
+        /**
+         * @brief Replace all occurrences of an escaped character by its unescaped version.
+         *
+         * @param s String containing zero or more occurrences of the character to replace
+         * @param toReplace C-style string to replace
+         * @param ch C-style string to replace with
+         *
+         * @pre toReplace must not be null
+         */
         inline void unescapeEscapeCharacter(
                 std::string& s, const char* const toReplace, const char* const ch)
         {
