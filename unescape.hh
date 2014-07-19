9c47f90d73d18db02128ea2d9ecbcacb8d4e70e0
Helmuth Ploner <h.ploner@axis-simulation.com>
2014-07-19 21:09:49 +0200

    refactoring

--- unescape.hh
+++ unescape.hh
@@ -1,6 +1,7 @@
 #ifndef JSON_HELPER_UNESCAPE_HH
 #define JSON_HELPER_UNESCAPE_HH
 
+#include <stdexcept>
 #include <string>
 
 #include "codepoint2utf8.hh"
@@ -8,28 +9,6 @@
 namespace JSON {
 
     namespace helper {
-
-        /**
-         * @brief Replace all occurrences of an escaped character by its unescaped version.
-         *
-         * @param s String containing zero or more occurrences of the character to replace
-         * @param toReplace C-style string to replace
-         * @param ch C-style string to replace with
-         *
-         * @pre toReplace must not be null
-         */
-        inline void unescapeEscapeCharacter(
-                std::string& s, const char* const toReplace, const char* const ch)
-        {
-            std::string::size_type n { };
-
-            while (true) {
-                n = s.find(toReplace, n);
-                if (n == std::string::npos) break;
-                s.replace(n, 2, ch);
-                ++n;
-            }
-        }
 
         inline void unescape(std::string& s)
         {
@@ -54,7 +33,10 @@ namespace JSON {
                                 static_cast<char16_t>(std::stoul(codePointStr, 0, 16)) };
                             result += codePoint2utf8(codePoint);
                             std::advance(citer, 4);
+                            break;
                         }
+                    default:
+                        throw std::runtime_error("Unexpected escape character in " + s);
                     }
                 }
                 else {
--- ut/unescapeescapecharactertest.cc
+++ ut/unescapeescapecharactertest.cc
@@ -2,7 +2,7 @@
 
 #include "unescape.hh"
 
-using JSON::helper::unescapeEscapeCharacter;
+using JSON::helper::unescape;
 
 class UnescapeEscapeCharacterTest : public CPPUNIT_NS::TestFixture
 {
@@ -35,56 +35,55 @@ CPPUNIT_TEST_SUITE_REGISTRATION(UnescapeEscapeCharacterTest);
 void UnescapeEscapeCharacterTest::testEmptyInput()
 {
     std::string s { R"()" };
-    unescapeEscapeCharacter(s, R"(\t)", "\t");
+    unescape(s);
     CPPUNIT_ASSERT_EQUAL(std::string{""}, s);
 }
 
 void UnescapeEscapeCharacterTest::testInputWithNoOccurrances()
 {
     std::string s { R"(nothing to replace)" };
-    unescapeEscapeCharacter(s, R"(\t)", "\t");
+    unescape(s);
     CPPUNIT_ASSERT_EQUAL(std::string{"nothing to replace"}, s);
 }
 
 void UnescapeEscapeCharacterTest::testOccurranceAtBegin()
 {
     std::string s { R"(\tto replace)" };
-    unescapeEscapeCharacter(s, R"(\t)", "\t");
+    unescape(s);
     CPPUNIT_ASSERT_EQUAL(std::string{"\tto replace"}, s);
 }
 
 void UnescapeEscapeCharacterTest::testOccurranceMiddle()
 {
     std::string s { R"(to \treplace)" };
-    unescapeEscapeCharacter(s, R"(\t)", "\t");
+    unescape(s);
     CPPUNIT_ASSERT_EQUAL(std::string{"to \treplace"}, s);
 }
 
 void UnescapeEscapeCharacterTest::testOccurranceAtEnd()
 {
     std::string s { R"(to replace\t)" };
-    unescapeEscapeCharacter(s, R"(\t)", "\t");
+    unescape(s);
     CPPUNIT_ASSERT_EQUAL(std::string{"to replace\t"}, s);
 }
 
 void UnescapeEscapeCharacterTest::testMultipleOccurrancesContiguous()
 {
     std::string s { R"(to \t\treplace)" };
-    unescapeEscapeCharacter(s, R"(\t)", "\t");
+    unescape(s);
     CPPUNIT_ASSERT_EQUAL(std::string{"to \t\treplace"}, s);
 }
 
 void UnescapeEscapeCharacterTest::testMultipleOccurrancesSpaced()
 {
     std::string s { R"(to \tre\tplace)" };
-    unescapeEscapeCharacter(s, R"(\t)", "\t");
+    unescape(s);
     CPPUNIT_ASSERT_EQUAL(std::string{"to \tre\tplace"}, s);
 }
 
 void UnescapeEscapeCharacterTest::testWithPartialMatch()
 {
     std::string s { R"(to \eplace)" };
-    unescapeEscapeCharacter(s, R"(\t)", "\t");
-    CPPUNIT_ASSERT_EQUAL(std::string{"to \\eplace"}, s);
+    CPPUNIT_ASSERT_THROW(unescape(s), std::runtime_error);
 }
 
