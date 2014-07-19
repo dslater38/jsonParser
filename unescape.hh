2919dc0d90fd62e192564b6f29e8b327c3405ad6
Helmuth Ploner <h.ploner@axis-simulation.com>
2014-07-19 20:38:34 +0200

    using different algorithm for unescaping
    old one produced wrong results

--- unescape.hh
+++ unescape.hh
@@ -1,3 +1,6 @@
+#ifndef JSON_HELPER_UNESCAPE_HH
+#define JSON_HELPER_UNESCAPE_HH
+
 #include <string>
 
 #include "codepoint2utf8.hh"
@@ -45,19 +48,40 @@ namespace JSON {
 
         inline void unescape(std::string& s)
         {
-            unescapeEscapeCharacter(s, "\\\"", "\"");
-            unescapeEscapeCharacter(s, "\\\\", "\\");
-            unescapeEscapeCharacter(s, "\\/", "/");
-            unescapeEscapeCharacter(s, "\\b", "\b");
-            unescapeEscapeCharacter(s, "\\f", "\f");
-            unescapeEscapeCharacter(s, "\\n", "\n");
-            unescapeEscapeCharacter(s, "\\r", "\r");
-            unescapeEscapeCharacter(s, "\\t", "\t");
+            std::string result;
 
-            unescapeUnicodeEscapeSequence(s);
+            auto citer = s.cbegin();
+            while (citer != s.cend()) {
+                const auto c = *citer++;
+                if (c == '\\') {
+                    switch (*citer++) {
+                    case '"': result += '\"'; break;
+                    case '\\': result += '\\'; break;
+                    case '/': result += '/'; break;
+                    case 'b': result += '\b'; break;
+                    case 'f': result += '\f'; break;
+                    case 'n': result += '\n'; break;
+                    case 't': result += '\t'; break;
+                    case 'r': result += '\r'; break;
+                    case 'u': {
+                            const std::string codePointStr { citer, citer + 4 };
+                            const char16_t codePoint {
+                                static_cast<char16_t>(std::stoul(codePointStr, 0, 16)) };
+                            result += codePoint2utf8(codePoint);
+                            std::advance(citer, 4);
+                        }
+                    }
+                }
+                else {
+                    result += c;
+                }
+            }
+
+            s = std::move(result);
         }
 
     }
 
 }
 
+#endif
